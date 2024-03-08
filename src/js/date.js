ea5a7c78fe5f0c008a60d43d3369995005d3b779
lx78WyY0J5 <9j2k37st@gmail.com>
2024-03-08 22:38:39 +0100

    update feeling of tabs

--- src/js/date.js
+++ src/js/date.js
@@ -2,23 +2,17 @@ var id = "date";
 
 setDateHTML();
 
-
-
 function setDateHTML() {
     var div = document.createElement("div");
     div.style = "max-width: 50px;";
     var date = document.createElement("p")
     date.id = id;
 
-    var dateTimeZone = document.createElement("p");
-    dateTimeZone.id = id + "TimeZone";
-
     var content = document.querySelector("#content-right");
     if (content != null) {
         content.appendChild(div);
 
         div.appendChild(date);
-        div.appendChild(dateTimeZone);
 
         setInterval(getDate, 250);
     }
@@ -26,11 +20,9 @@ function setDateHTML() {
 
 function getDate() {    
     var date = new Date();
-    var message = addZero(date.getDate()) + "/" + addZero(date.getMonth() + 1) + " " + date.getFullYear() + " " + date.getHours() + ":" + addZero(date.getUTCMinutes());
-    console.log(message);
+    var message = addZero(date.getDate()) + "/" + addZero(date.getMonth() + 1) + " " + date.getFullYear() + " " + addZero(date.getHours()) + ":" + addZero(date.getUTCMinutes());
 
     document.getElementById(id).textContent = message;
-    document.getElementById(id + "TimeZone").textContent = (Intl.DateTimeFormat().resolvedOptions().timeZone).replaceAll("/"," ");
 }
 
 function addZero(i) {
